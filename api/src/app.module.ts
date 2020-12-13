c8d80b4f40a3b58cc3c7f1244b6dc4c10d40a465
Gonzalo Barba <zalo250@gmail.com>
2020-12-13 16:57:17 +0100

    Updating config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,24 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserModule } from './user/user.module';
 import { PetModule } from './pet/pet.module';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from './config/constants';
 
 @Module({
   imports: [
     UserModule,
     PetModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'petsdb',
-      entities: [__dirname + './**/**/*.entity{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-      logger: 'file',
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>(DATABASE_HOST),
+        port: parseInt(config.get<string>(DATABASE_PORT), 10),
+        username: config.get<string>(DATABASE_USERNAME),
+        password: config.get<string>(DATABASE_PASSWORD),
+        database: config.get<string>(DATABASE_NAME),
+        entities: [__dirname + './**/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file',
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
   ],
 })
